4b876d630a89cf4fbc5d46ec88ff56a5ab9a050b
GBY <kwancr92@gmail.com>
2018-12-27 16:14:30 +0800

    back to funny rust

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("This is a guessing game");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    };
+    println!("Secret Number is {}", secret_number);
+}
